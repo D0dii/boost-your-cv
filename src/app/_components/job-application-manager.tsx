93ee80537056e031218637843860b573f2e07a54
D0dii <marcindolatowski123@gmail.com>
2025-04-17 12:59:15 +0200

    feat: add form to upload job description and cv

--- src/app/_components/job-application-manager.tsx
+++ src/app/_components/job-application-manager.tsx
@@ -1,9 +1,67 @@
 "use client";
 
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@radix-ui/react-label";
+import { useRef, useState, type ChangeEvent } from "react";
+import pdfToText from "react-pdftotext";
 
 export function JobApplicationManager() {
-  const [jobDescription, setJobDescription] = useState("");
   const [cv, setCv] = useState<File | null>(null);
-  return <div>dawaj cv</div>;
+  const [cvInvalidFormatError, setCvInvalidFormatError] = useState("");
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const submitForm = async (formData: FormData) => {
+    if (cv) {
+      console.log(formData.get("job-description"));
+      const cvText = await pdfToText(cv);
+      console.log(cvText);
+    }
+  };
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file && file.type === "application/pdf") {
+      setCvInvalidFormatError("");
+      setCv(file);
+    } else {
+      if (fileInputRef.current) {
+        fileInputRef.current.value = "";
+      }
+      setCv(null);
+      setCvInvalidFormatError("Please upload pdf file");
+    }
+  };
+  return (
+    <form className="flex flex-col items-center gap-4" action={submitForm}>
+      <div className="flex w-full flex-col gap-1">
+        <Label htmlFor="job-description">Job Description</Label>
+        <Textarea
+          id="job-description"
+          name="job-description"
+          placeholder="Paste your job description"
+          className="max-h-[300px] max-w-[350px]"
+          required
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <Label htmlFor="picture">Your CV</Label>
+        <Input
+          ref={fileInputRef}
+          id="picture"
+          name="picture"
+          type="file"
+          required
+          onChange={handleFileChange}
+          accept="application/pdf"
+        />
+        {cvInvalidFormatError ? (
+          <p className="text-sm text-red-400">{cvInvalidFormatError}</p>
+        ) : null}
+      </div>
+      <Button type="submit" className="w-[150px]">
+        Submit
+      </Button>
+    </form>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { JobApplicationManager } from "./_components/job-application-manager";
 
 export default async function Home() {
   return (
-    <main className="mt-14 flex flex-col items-center px-12">
+    <main className="mt-14 flex flex-col items-center px-12 md:px-72">
       <h1 className="text-4xl font-semibold">Welcome to boost your cv</h1>
       <h2 className="mt-4 mb-24 text-xl">
         Please fill the job description and upload your cv and we will check
